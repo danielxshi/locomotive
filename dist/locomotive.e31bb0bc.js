1b08c976c06b5b94f25d3e3952ee11641ccb01ea
Daniel Shi <xdshi@sfu.ca>
2021-04-12 12:53:12 -0700

    Compressed images and added index mobile hero

--- dist/locomotive.e31bb0bc.js
+++ dist/locomotive.e31bb0bc.js
@@ -3310,7 +3310,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52038" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59466" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
