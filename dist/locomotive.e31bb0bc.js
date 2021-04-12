7f97d1f8444a0b90f4a95fa88eec857bc5bdb8db
Daniel Shi <xdshi@sfu.ca>
2021-04-12 14:28:06 -0700

    added more project navigation with mobile, tablet, desktop media queries

--- dist/locomotive.e31bb0bc.js
+++ dist/locomotive.e31bb0bc.js
@@ -3310,7 +3310,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59466" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59470" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
